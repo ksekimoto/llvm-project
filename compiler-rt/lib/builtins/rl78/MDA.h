c040c8cabb28067eb569b82e9ca5d314f7479fc8
ksekimoto <ksekimoto@users.noreply.github.com>
2023-03-29 16:01:04 +0900

    Add compiler-rt files

---
+++ compiler-rt/lib/builtins/rl78/MDA.h
@@ -0,0 +1,9 @@
+#ifdef __MDA_ENABLED__
+#define MDUC 0xf00e8
+#define MDAL 0xffff0
+#define MDAH 0xffff2
+#define MDBL 0xffff6
+#define MDBH 0xffff4
+#define MDCL 0xf00e0
+#define MDCH 0xf00e2
+#endif
